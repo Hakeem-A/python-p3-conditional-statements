572595fc401976fadfd04b4e044c06df7b47c4e2
Abdi Mohamed <bashanesix@gmail.com>
2025-02-20 11:00:18 +0300

    all tests passed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -16,13 +16,29 @@ def hows_the_weather(temperature):
         return "It's too dang hot out there!"
     else:
         return "It's perfect out there!"
-    # your code here
-    pass
+    
+
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+    
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        return num1 / num2
+    else:
+        print('Invalid operation!')
+        return None 
